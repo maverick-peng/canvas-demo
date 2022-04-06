6eade44187da34ab46ec521c9091ef72ea06edb0
maverick-peng <alanpeng.ee00@g2.nctu.edu.tw>
2022-04-06 09:20:14 +0800

    chores: deploy test

--- src/main.ts
+++ src/main.ts
@@ -1,140 +1,161 @@
 import './style.css';
+import bgImage from './assets/bg.png';
+
+import { fromEvent, switchMap, takeUntil, tap } from 'rxjs';
 
 var canvas = document.querySelector<HTMLCanvasElement>('canvas')!;
 var context = canvas.getContext('2d')!;
 canvas.width = 600;
 canvas.height = 400;
 
-// lazy programmers globals
-var scale = 1;
-var wx = 0; // world zoom origin
-var wy = 0;
-var sx = 0; // mouse screen pos
-var sy = 0;
+let scale = 1.0;
+
+let originX = 0;
+let originY = 0;
+let screenOriginX = 0;
+let screenOriginY = 0;
 
 interface Mouse {
-  x?: number;
-  y?: number;
-  rx?: number;
-  ry?: number;
-  button?: number;
+  screenX: number;
+  screenY: number;
+  realX: number;
+  realY: number;
   bounds?: DOMRect;
 }
-var mouse: Mouse = {};
-mouse.x = 0; // pixel pos of mouse
-mouse.y = 0;
-mouse.rx = 0; // mouse real (world) pos
-mouse.ry = 0;
-mouse.button = 0;
 
-function zoomed(number: number) {
-  // just scale
+const mouse: Mouse = {
+  screenX: 0,
+  screenY: 0,
+  realX: 0,
+  realY: 0,
+};
+
+const image = new Image();
+image.src = bgImage;
+image.onload = function () {
+  render();
+};
+
+// drag
+fromEvent<MouseEvent>(canvas, 'mousedown')
+  .pipe(
+    tap((event) => {
+      mouse.bounds = canvas.getBoundingClientRect();
+      mouse.screenX = event.clientX - mouse.bounds.left;
+      mouse.screenY = event.clientY - mouse.bounds.top;
+      mouse.realX = screenToRealX(mouse.screenX);
+      mouse.realY = screenToRealY(mouse.screenY);
+      console.log(mouse);
+    }),
+    switchMap(() =>
+      fromEvent<MouseEvent>(canvas, 'mousemove').pipe(
+        tap((innerEvent) => {
+          const lastX = mouse.realX;
+          const lastY = mouse.realY;
+
+          mouse.bounds = canvas.getBoundingClientRect();
+          mouse.screenX = innerEvent.clientX - mouse.bounds.left;
+          mouse.screenY = innerEvent.clientY - mouse.bounds.top;
+          mouse.realX = screenToRealX(mouse.screenX);
+          mouse.realY = screenToRealY(mouse.screenY);
+
+          const dx = mouse.realX - lastX;
+          const dy = mouse.realY - lastY;
+          originX -= dx;
+          originY -= dy;
+          mouse.realX = screenToRealX(mouse.screenX);
+          mouse.realY = screenToRealY(mouse.screenY);
+
+          render();
+        }),
+        takeUntil(fromEvent<MouseEvent>(canvas, 'mouseout')),
+        takeUntil(fromEvent<MouseEvent>(canvas, 'mouseup'))
+      )
+    )
+  )
+  .subscribe();
+
+// wheel
+fromEvent<WheelEvent>(canvas, 'wheel', { passive: false })
+  .pipe(
+    tap((event) => {
+      event.preventDefault();
+
+      if (event.deltaY < 0) {
+        scale = Math.min(5, scale * 1.1); // zoom in
+      } else {
+        scale = Math.max(0.1, scale * (1 / 1.1)); // zoom out is inverse of zoom in
+      }
+
+      mouse.bounds = canvas.getBoundingClientRect();
+      mouse.screenX = event.clientX - mouse.bounds.left;
+      mouse.screenY = event.clientY - mouse.bounds.top;
+      mouse.realX = screenToRealX(mouse.screenX);
+      mouse.realY = screenToRealY(mouse.screenY);
+      console.log(mouse);
+
+      originX = mouse.realX;
+      originY = mouse.realY;
+      screenOriginX = mouse.screenX;
+      screenOriginY = mouse.screenY;
+
+      mouse.realX = screenToRealX(mouse.screenX);
+      mouse.realY = screenToRealY(mouse.screenY);
+
+      render();
+    })
+  )
+  .subscribe();
+
+/* Coordinates trasformation functions */
+
+// just scale
+function zoom(number: number) {
   return Math.floor(number * scale);
 }
 // converts from world coord to screen pixel coord
-function zoomedX(number: number) {
-  // scale & origin X
-  return Math.floor((number - wx) * scale + sx);
+function realToScreenX(number: number) {
+  return Math.floor((number - originX) * scale + screenOriginX);
 }
 
-function zoomedY(number: number) {
-  // scale & origin Y
-  return Math.floor((number - wy) * scale + sy);
+function realToScreenY(number: number) {
+  return Math.floor((number - originY) * scale + screenOriginY);
 }
-
-// Inverse does the reverse of a calculation. Like (3 - 1) * 5 = 10   the inverse is 10 * (1/5) + 1 = 3
-// multiply become 1 over ie *5 becomes * 1/5  (or just /5)
-// Adds become subtracts and subtract become add.
-// and what is first become last and the other way round.
 
 // inverse function converts from screen pixel coord to world coord
-function zoomedX_INV(number: number) {
-  // scale & origin INV
-  return Math.floor((number - sx) * (1 / scale) + wx);
-  // or return Math.floor((number - sx) / scale + wx);
+function screenToRealX(number: number) {
+  return Math.floor((number - screenOriginX) * (1 / scale) + originX);
 }
 
-function zoomedY_INV(number: number) {
-  // scale & origin INV
-  return Math.floor((number - sy) * (1 / scale) + wy);
-  // or return Math.floor((number - sy) / scale + wy);
+function screenToRealY(number: number) {
+  return Math.floor((number - screenOriginY) * (1 / scale) + originY);
 }
 
-// draw everything in pixels coords
-function draw() {
+function render() {
   context.clearRect(0, 0, canvas.width, canvas.height);
-
+  context.drawImage(
+    image,
+    realToScreenX(0),
+    realToScreenY(0),
+    zoom(image.naturalWidth),
+    zoom(image.naturalHeight)
+  );
   context.beginPath();
-  context.rect(zoomedX(50), zoomedY(50), zoomed(100), zoomed(100));
+  context.rect(realToScreenX(50), realToScreenY(50), zoom(100), zoom(100));
   context.fillStyle = 'skyblue';
   context.fill();
 
   context.beginPath();
-  context.arc(zoomedX(350), zoomedY(250), zoomed(50), 0, 2 * Math.PI, false);
+  context.arc(
+    realToScreenX(350),
+    realToScreenY(250),
+    zoom(50),
+    0,
+    2 * Math.PI,
+    false
+  );
   context.fillStyle = 'green';
   context.fill();
+
+  // window.requestAnimationFrame(render);
 }
-// wheel event must not be passive to allow default action to be prevented
-canvas.addEventListener('wheel', trackWheel, { passive: false });
-canvas.addEventListener('mousemove', move);
-canvas.addEventListener('mousedown', move);
-canvas.addEventListener('mouseup', move);
-canvas.addEventListener('mouseout', move); // to stop mouse button locking up
-
-function move(event: MouseEvent) {
-  // mouse move event
-  if (event.type === 'mousedown') {
-    mouse.button = 1;
-  } else if (event.type === 'mouseup' || event.type === 'mouseout') {
-    mouse.button = 0;
-  }
-
-  mouse.bounds = canvas.getBoundingClientRect();
-  mouse.x = event.clientX - mouse.bounds.left;
-  mouse.y = event.clientY - mouse.bounds.top;
-  var xx = mouse.rx || 0; // get last real world pos of mouse
-  var yy = mouse.ry || 0;
-
-  mouse.rx = zoomedX_INV(mouse.x); // get the mouse real world pos via inverse scale and translate
-  mouse.ry = zoomedY_INV(mouse.y);
-  if (mouse.button === 1) {
-    // is mouse button down
-    wx -= mouse.rx - xx; // move the world origin by the distance
-    // moved in world coords
-    wy -= mouse.ry - yy;
-    // recaculate mouse world
-    mouse.rx = zoomedX_INV(mouse.x);
-    mouse.ry = zoomedY_INV(mouse.y);
-  }
-  // draw();
-  console.log(mouse);
-  console.log(wx, wy, sx, sy);
-}
-
-function trackWheel(e: WheelEvent) {
-  if (e.deltaY < 0) {
-    scale = Math.min(5, scale * 1.1); // zoom in
-  } else {
-    scale = Math.max(0.1, scale * (1 / 1.1)); // zoom out is inverse of zoom in
-  }
-  wx = mouse.rx!; // set world origin
-  wy = mouse.ry!;
-  sx = mouse.x!; // set screen origin
-  sy = mouse.y!;
-  mouse.rx = zoomedX_INV(mouse.x!); // recalc mouse world (real) pos
-  mouse.ry = zoomedY_INV(mouse.y!);
-
-  // draw();
-  e.preventDefault(); // stop the page scrolling
-  console.log(mouse);
-  console.log(wx, wy, sx, sy);
-  console.log(scale);
-}
-// draw();
-
-function animate() {
-  draw();
-  requestAnimationFrame(animate);
-}
-
-animate();
