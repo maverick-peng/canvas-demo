5e77258c4f7d371c979404b8b47e8c3054110a32
maverick-peng <maverick.peng@hyweb.com.tw>
2022-04-11 14:01:37 +0800

    add: handle touch behavior

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
 import './style.css';
 import bgImage from './assets/bg.png';
 
-import { fromEvent, switchMap, takeUntil, tap } from 'rxjs';
+import { fromEvent, iif, switchMap, takeUntil, tap } from 'rxjs';
+
+/**
+ * Extend CanvasRenderingContext2D class so that it could draw rectangle with rounded corners
+ */ 
+ CanvasRenderingContext2D.prototype.roundRect = function(x: number, y: number, w: number, h: number, r: number) {
+  this.beginPath();
+  this.moveTo(x + r, y);
+  this.arcTo(x + w, y, x + w, y + h, r);
+  this.arcTo(x + w, y + h, x, y + h, r);
+  this.arcTo(x, y + h, x, y, r);
+  this.arcTo(x, y, x + w, y, r);
+  this.fill();
+  return this;
+}
 
 var canvas = document.querySelector<HTMLCanvasElement>('canvas')!;
 var context = canvas.getContext('2d')!;
-canvas.width = 600;
-canvas.height = 400;
+canvas.width = window.innerWidth * 0.7;
+canvas.height = window.innerHeight;
 
-let scale = 1.0;
+let scale = canvas.width / 1920;
 
 let originX = 0;
 let originY = 0;
@@ -36,38 +50,47 @@ image.onload = function () {
   render();
 };
 
+/**
+ * Update screen coordinates (screenX, screenY) by given client position (clientX, clientY)
+ * @param event 
+ */
+function updateScreenCoords(clientX: number, clientY: number) {
+  mouse.bounds = canvas.getBoundingClientRect();
+  mouse.screenX = clientX - mouse.bounds.left;
+  mouse.screenY = clientY - mouse.bounds.top;
+}
+
+function handleDrag(clientX: number, clientY: number) {
+  const lastX = mouse.realX;
+  const lastY = mouse.realY;
+
+  updateScreenCoords(clientX, clientY);
+  mouse.realX = screenToRealX(mouse.screenX);
+  mouse.realY = screenToRealY(mouse.screenY);
+
+  const dx = mouse.realX - lastX;
+  const dy = mouse.realY - lastY;
+  originX -= dx;
+  originY -= dy;
+  mouse.realX = screenToRealX(mouse.screenX);
+  mouse.realY = screenToRealY(mouse.screenY);
+
+  render();
+}
+
 // drag
 fromEvent<MouseEvent>(canvas, 'mousedown')
   .pipe(
     tap((event) => {
-      mouse.bounds = canvas.getBoundingClientRect();
-      mouse.screenX = event.clientX - mouse.bounds.left;
-      mouse.screenY = event.clientY - mouse.bounds.top;
+      updateScreenCoords(event.clientX, event.clientY);
       mouse.realX = screenToRealX(mouse.screenX);
       mouse.realY = screenToRealY(mouse.screenY);
-      console.log(mouse);
     }),
     switchMap(() =>
       fromEvent<MouseEvent>(canvas, 'mousemove').pipe(
-        tap((innerEvent) => {
-          const lastX = mouse.realX;
-          const lastY = mouse.realY;
-
-          mouse.bounds = canvas.getBoundingClientRect();
-          mouse.screenX = innerEvent.clientX - mouse.bounds.left;
-          mouse.screenY = innerEvent.clientY - mouse.bounds.top;
-          mouse.realX = screenToRealX(mouse.screenX);
-          mouse.realY = screenToRealY(mouse.screenY);
-
-          const dx = mouse.realX - lastX;
-          const dy = mouse.realY - lastY;
-          originX -= dx;
-          originY -= dy;
-          mouse.realX = screenToRealX(mouse.screenX);
-          mouse.realY = screenToRealY(mouse.screenY);
-
-          render();
-        }),
+        tap((innerEvent) => 
+          handleDrag(innerEvent.clientX, innerEvent.clientY)
+        ),
         takeUntil(fromEvent<MouseEvent>(canvas, 'mouseout')),
         takeUntil(fromEvent<MouseEvent>(canvas, 'mouseup'))
       )
@@ -87,9 +110,7 @@ fromEvent<WheelEvent>(canvas, 'wheel', { passive: false })
         scale = Math.max(0.1, scale * (1 / 1.1)); // zoom out is inverse of zoom in
       }
 
-      mouse.bounds = canvas.getBoundingClientRect();
-      mouse.screenX = event.clientX - mouse.bounds.left;
-      mouse.screenY = event.clientY - mouse.bounds.top;
+      updateScreenCoords(event.clientX, event.clientY);
       mouse.realX = screenToRealX(mouse.screenX);
       mouse.realY = screenToRealY(mouse.screenY);
       console.log(mouse);
@@ -104,8 +125,54 @@ fromEvent<WheelEvent>(canvas, 'wheel', { passive: false })
 
       render();
     })
-  )
-  .subscribe();
+  
+  
+  ).subscribe();
+
+// touch
+let originalDistance = 0;
+fromEvent<TouchEvent>(canvas, 'touchstart')
+  .pipe(
+    tap(event => {
+      updateScreenCoords(event.touches[0].clientX, event.touches[0].clientY);
+      mouse.realX = screenToRealX(mouse.screenX);
+      mouse.realY = screenToRealY(mouse.screenY);
+
+
+      if (event.touches.length === 2) {
+        originalDistance = Math.hypot(
+          event.touches[0].pageX - event.touches[1].pageX,
+          event.touches[0].pageY - event.touches[1].pageY
+        );
+      }
+    }),
+    switchMap(event => iif(
+      () => event.touches.length === 2,
+      // pinch
+      fromEvent<TouchEvent>(canvas, 'touchmove').pipe(
+        tap(innerEvent => {
+          const distance = Math.hypot(
+            innerEvent.touches[0].pageX - innerEvent.touches[1].pageX,
+            innerEvent.touches[0].pageY - innerEvent.touches[1].pageY
+          );
+
+          scale = (distance / originalDistance);
+
+          render();
+        }),
+        takeUntil(fromEvent(canvas, 'mouseend'))
+      ),
+      // slide
+      fromEvent<TouchEvent>(canvas, 'touchmove').pipe(
+        tap(innerEvent => 
+          handleDrag(innerEvent.touches[0].clientX, innerEvent.touches[0].clientY)
+        ),
+        takeUntil(fromEvent(canvas, 'touchend')),
+        takeUntil(fromEvent(canvas, 'touchcancel'))
+      )
+    )),
+
+  ).subscribe();
 
 /* Coordinates trasformation functions */
 
@@ -133,29 +200,22 @@ function screenToRealY(number: number) {
 
 function render() {
   context.clearRect(0, 0, canvas.width, canvas.height);
+  // context.clae
+
+  // draw background
   context.drawImage(
     image,
     realToScreenX(0),
     realToScreenY(0),
     zoom(image.naturalWidth),
-    zoom(image.naturalHeight)
-  );
-  context.beginPath();
-  context.rect(realToScreenX(50), realToScreenY(50), zoom(100), zoom(100));
-  context.fillStyle = 'skyblue';
-  context.fill();
+    zoom(image.naturalHeight),
+    );
+  
+  // change fill style
+  context.fillStyle = 'lineargradiant(0, 0, blue, red)';
 
-  context.beginPath();
-  context.arc(
-    realToScreenX(350),
-    realToScreenY(250),
-    zoom(50),
-    0,
-    2 * Math.PI,
-    false
-  );
-  context.fillStyle = 'green';
-  context.fill();
-
-  // window.requestAnimationFrame(render);
+  // draw a rectangle with rounded corners
+  context.roundRect(realToScreenX(194), realToScreenY(350), zoom(100), zoom(100), zoom(10));
 }
+
+
